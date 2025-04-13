d70e7884c56175dfb409d831575dec685da6b131
munezero-grace <munezero.grace911@gmail.com>
2025-04-13 19:46:58 +0200

    feat: set up Sequelize and connect to Neon DB
    Initialized Sequelize configuration and established database connection to Neon.
    Created initial User model with fields as per project requirements.

---
+++ models/User.js
@@ -0,0 +1,44 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database.js";
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  registrationNumber: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  dateOfBirth: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM("student", "admin"),
+    defaultValue: "student",
+  },
+}, {
+  timestamps: true,
+});
+
+export default User;
